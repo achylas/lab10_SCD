ba9dd733b782135c6b0b9694a462eb69191fa54f
achylas <inshk17@gmail.com>
2025-03-27 10:34:59 +0500

    new

--- ProxyPatternExample/src/Main.java
+++ ProxyPatternExample/src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         // Creating users
-        User adminUser = new User("AdminUser", true);
-        User regularUser = new User("RegularUser", false);
+        User adminUser = new User("Admin User", true);
+        User regularUser = new User("Regular User", false);
 
         // Creating document proxies for both users
         Document adminDocument = new DocumentProxy("SecretFile.pdf", adminUser);
         Document userDocument = new DocumentProxy("SecretFile.pdf", regularUser);
 
         // Trying to access the document
-        System.out.println("\nAdmin User trying to access:");
+        System.out.println("\nAdmin User is trying to access:");
         adminDocument.display();
 
-        System.out.println("\nRegular User trying to access:");
+        System.out.println("\nRegular User is trying to access:");
         userDocument.display();
     }
 }
